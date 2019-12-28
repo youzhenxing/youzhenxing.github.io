55f23bea682c0027e757c6fc6102b61de9bd122a
youzhenxing <youzx163@163.com>
2019-12-28 12:52:57 +0800

    refactor update_setting.sh

--- settings/update_setting.sh
+++ settings/update_setting.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 git clone https://github.com/probberechts/hexo-theme-cactus.git themes/cactus
+cp settings/cactus/footer.ejs themes/cactus/layout/_partial/footer.ejs
 cp settings/cactus.yml themes/cactus/_config.yml
 cp settings/site.yml _config.yml
